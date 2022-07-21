14b6474f3da34ae88de918dbb6e785b7dbd9b020
Dean Davids <ddavids@student.wethinkcode.co.za>
2022-07-21 22:35:35 +0200

    added new token method for cookies and added db migration for remember digest

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_095749) do
+ActiveRecord::Schema.define(version: 2022_07_21_202312) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
